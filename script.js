8a0c986e0fd837aa487d6dd415caa93bb2fb911c
BALAJI GOUNI <112768933+Bvprasad17@users.noreply.github.com>
2023-03-22 01:03:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 function findFactors() {
-  var number = BigInt(document.getElementById("number").value);
-  var factors = [];
-
-  for (var i = BigInt(1); i * i <= number; i++) {
-    if (number % i === 0n) {
-      factors.push(i);
-      if (i * i !== number) {
-        factors.push(number / i);
+    var number = document.getElementById("number").value;
+    var factors = [];
+  
+    for (var i = 1; i <= Math.sqrt(number); i++) {
+      if (number % i === 0) {
+        factors.push(i);
+        if (i !== Math.sqrt(number)) {
+          factors.push(number / i);
+        }
       }
     }
+    
+    factors.sort(function(a, b) { return a - b; });
+    
+    var factorList = document.getElementById("factors");
+    factorList.innerHTML = factors.join(", "); 
   }
-  var factorList = document.getElementById("factors");
-  factorList.innerHTML = factors.join(", "); 
-}
+  
