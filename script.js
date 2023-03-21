3c95d76a2f11e24f669b1780e31c8375f55ea348
BALAJI GOUNI <112768933+Bvprasad17@users.noreply.github.com>
2023-03-22 00:54:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,9 +10,6 @@ function findFactors() {
       }
     }
   }
-
-  factors.sort(function(a, b) { return a - b; });
-
   var factorList = document.getElementById("factors");
   factorList.innerHTML = factors.join(", "); 
 }
