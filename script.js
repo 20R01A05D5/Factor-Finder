227e9fa07ebe01d700721c46f023cbc4672129dd
BALAJI GOUNI <112768933+Bvprasad17@users.noreply.github.com>
2023-04-10 19:13:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function findFactors() {
     var number = document.getElementById("number").value;
     var factors = [];
+    var count = 0;
   
     for (var i = 1; i <= Math.sqrt(number); i++) {
       if (number % i === 0) {
         factors.push(i);
+        count++;
         if (i !== Math.sqrt(number)) {
           factors.push(number / i);
+          count++;
         }
       }
     }
@@ -15,5 +18,7 @@ function findFactors() {
     
     var factorList = document.getElementById("factors");
     factorList.innerHTML = factors.join(", "); 
+    
+    var countContainer = document.getElementById("count");
+    countContainer.innerHTML = "Number of factors: " + count;
   }
-  
